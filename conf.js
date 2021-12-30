9c8d5143688642c6d7d03ceaac64251da68584f3
zj029 <73580692+zj029@users.noreply.github.com>
2021-12-30 16:11:36 +0800

    Add files via upload

--- conf.js
+++ conf.js
@@ -2,16 +2,23 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '111',
+  ver: '112',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
     enable: true,
-    ver: 63
+    ver: 66
   },
 
   // 节点配置
-  node_map: {
+  anode_map: {
+    'ademo-hk': {
+      label: 'yd029-c698点',
+      lines: {
+        'ancient-king-c698.029.workers.dev': 1,
+        'crimson-snow-3307.029sina.workers.dev': 2,
+      }
+   },
     'demo-hk': {
       label: 'yd029-2点',
       lines: {
@@ -19,6 +26,12 @@ jsproxy_config({
         'crimson-snow-3307.029sina.workers.dev': 2,
       }
    },
+    'bdemo-sg': {
+      label: '001.029-029',
+      lines: {
+        '001.001.029.workers.dev': 1,
+      }
+    },
     'demo-sg': {
       label: 'googlehk-jz126',
       lines: {
@@ -31,10 +44,40 @@ jsproxy_config({
         '1.wv1.workers.dev': 1,
       }
    },
+    'cdshjh': {
+      label: 'c1112v-029',
+      lines: {
+        'production.03.029.workers.dev': 1,
+      }
+   },
+    'ddshjh': {
+      label: 'd1112v-029',
+      lines: {
+        '002.kmw.029.workers.dev': 1,
+      }
+    },
+    'edshjh': {
+      label: 'e029hh.029hh',
+      lines: {
+        'production.029hh.029hh.workers.dev': 1,
+      }
+    },
     'dshjh': {
       label: '1112v-kmw',
       lines: {
         '1112v.029.workers.dev': 1,
+      }
+     },
+    'fsffs-ij': {
+      label: 'fyahoob-126',
+      lines: {
+        'yahoob.jz126.workers.dev': 1,
+      }
+    },
+    'gsffs-ij': {
+      label: '029-hh',
+      lines: {
+        'production.0.029hh.workers.dev': 1,
       }
     },
     'sffs-ij': {
@@ -136,6 +179,11 @@ jsproxy_config({
     'cfworker': {
       label: 'jz',
       lines: {
+        'googlehk.jz126.workers.dev': 1,
+        'googlea.jz126.workers.dev': 1,
+        'yahooa.jz126.workers.dev': 1,
+        'production.0.029hh.workers.dev': 1,
+        'red-breeze-27a6.693.workers.dev': 1,
         'jj.693.workers.dev': 1,
         'j.xj2.workers.dev': 1,
         'damp-shape-64ee.693.workers.dev': 1,
@@ -164,7 +212,7 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  node_default: 'mysite',
+  node_default: 'anode_map',
   // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
